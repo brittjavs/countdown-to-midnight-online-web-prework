b8dfa4a310d4c12afa660247e8d8c66804bed5ab
Brittany Javalera <brittjavalera@gmail.com>
2019-05-02 18:04:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,9 @@
 #write your code here
 
-def countdown(number)
+def countdown
   number = 0
   while number < 10
   puts "#{number}"
-  if number == 0
-    puts "HAPPY NEW YEAR!"
   number -= 1
   end
 end
